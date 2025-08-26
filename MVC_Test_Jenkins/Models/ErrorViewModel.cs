3f82daa10febe2c076c16ee3a76fe3a54050e2bd
P-Nastia <nastia.pyvovarchuck@gmail.com>
2025-08-26 14:26:40 +0300

    Working Jenkins

---
+++ MVC_Test_Jenkins/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_Test_Jenkins.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
